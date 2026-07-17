64e9c685fc660995dfc7ce918e6e1d5552b7d323
chichi-9527 <wu_coffee@126.com>
2026-07-18 02:01:36 +0800

    update cmake file

--- Engine/src/engine_test.cpp
+++ Engine/src/engine_test.cpp
@@ -2,6 +2,7 @@
 
 #include "slang_c_test.h"
 
+#include <btBulletDynamicsCommon.h>
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 #include <assimp/version.h>
@@ -38,6 +39,28 @@ namespace INVENT
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
     std::cout << "[Engine] Vulkan 核心鏈結成功！支援擴展數量: " << extensionCount << std::endl;
+
+
+    // 4. bullet3
+    std::cout << "[Engine] 正在配置物理世界..." << std::endl;
+
+    // 建立最基礎的 Bullet 物理世界四件套
+    auto* collisionConfiguration = new btDefaultCollisionConfiguration(); //[INDEX_1.2.3]
+    auto* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+    auto* overlappingPairCache = new btDbvtBroadphase();
+    auto* solver = new btSequentialImpulseConstraintSolver;
+    auto* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+
+    // 設置重力 (向下 9.8)
+    dynamicsWorld->setGravity(btVector3(0, -9.8f, 0));
+    std::cout << "[Engine] Bullet 物理引擎鏈結成功！重力設定完畢。" << std::endl;
+
+    // 清理記憶體
+    delete dynamicsWorld;
+    delete solver;
+    delete overlappingPairCache;
+    delete dispatcher;
+    delete collisionConfiguration;
     }
     
 } // namespace INVENT
