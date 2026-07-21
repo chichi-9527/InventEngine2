0548a3a5f79b024d27a069714e344b5a4df176b9
chichi-9527 <wu_coffee@126.com>
2026-07-21 15:45:25 +0800

    修改之前编译错误的问题

---
+++ EngineEditor/src/Public/EditorConfig.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+
+// #ifndef INVENT_EDITOR
+// #define INVENT_EDITOR 0
+// #endif
+
+#define DevelopmentMode 1
+
+// 开发模式时，更简单的更换是否开启编辑器
+#if DevelopmentMode
+#define WITH_EDITOR 1
+#endif
+#define WITH_EDITOR 0
+#endif
