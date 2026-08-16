ff6626fd8bd84ace1a9918d798147e5c446ad397
chichi-9527 <wu_coffee@126.com>
2026-08-16 20:47:54 +0800

    修改了结构

--- Engine/Private/IVulkan/IVulkanGlobalTexture.h
+++ Engine/Private/IVulkan/IVulkanGlobalTexture.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "IBitArray.h"
+#include "VulkanConfig.h"
 
 #include <cstdint>
 #include <string>
@@ -30,31 +31,6 @@ namespace INVENT
 		{
 			VkImage Image = VK_NULL_HANDLE;
 			VkImageView ImageView = VK_NULL_HANDLE;
-		};
-
-		struct Texture2DHandle
-		{
-			IHandle handle{};
-			std::uint64_t version{ 0 };
-
-			Texture2DHandle() = default;
-			constexpr Texture2DHandle(size_t h)
-				: handle(h){}
-			constexpr Texture2DHandle(size_t h, std::uint32_t v)
-				: handle(h), version(v) {}
-			Texture2DHandle(const Texture2DHandle&) = default;
-			Texture2DHandle(Texture2DHandle&&) noexcept = default;
-
-			Texture2DHandle& operator=(const Texture2DHandle&) = default;
-			Texture2DHandle& operator=(Texture2DHandle&&) noexcept = default;
-
-			friend bool operator==(const Texture2DHandle& handle1, const Texture2DHandle& handle2)
-			{
-				return handle1.handle == handle2.handle &&
-					handle1.version == handle2.version;
-			}
-
-			bool IsValid() const noexcept { return handle.IsValid(); }
 		};
 
 		struct IVulkanTexture2DHandle
@@ -132,10 +108,10 @@ namespace INVENT
 	private:
 
 		using TextureNameMap = std::unordered_map < std::string,
-			Texture2DHandle,
+			IVulkan::Texture2DHandle,
 			std::hash<std::string>,
 			std::equal_to<std::string>,
-			IMemPoolAllocatorOnlyFixedBlock<std::pair<const std::string, Texture2DHandle>>>;
+			IMemPoolAllocatorOnlyFixedBlock<std::pair<const std::string, IVulkan::Texture2DHandle>>>;
 		using TextureHandleNameMap = std::unordered_map<
 			size_t,
 			std::string,
@@ -151,37 +127,37 @@ namespace INVENT
 		static IVulkanTexture2DManagement& Instance();
 		void Clear();
 
-		static constexpr Texture2DHandle GetWhitePixel() noexcept { return 0; }
-		static constexpr Texture2DHandle GetBlackPixel() noexcept { return 1; }
-		static constexpr Texture2DHandle GetNormalPixel() noexcept { return 2; }
+		static constexpr IVulkan::Texture2DHandle GetWhitePixel() noexcept { return 0; }
+		static constexpr IVulkan::Texture2DHandle GetBlackPixel() noexcept { return 1; }
+		static constexpr IVulkan::Texture2DHandle GetNormalPixel() noexcept { return 2; }
 
-		Texture2DHandle AllocateTextureHandle();
+		IVulkan::Texture2DHandle AllocateTextureHandle();
 		/// <returns> 失败时会返回无效的句柄 </returns>
-		Texture2DHandle AddTexture2D(const std::string& path, TextureType texture_type = TextureType::TYPE_Undefined, bool is_create_mipmaps = true);
+		IVulkan::Texture2DHandle AddTexture2D(const std::string& path, TextureType texture_type = TextureType::TYPE_Undefined, bool is_create_mipmaps = true);
 		/// <returns> 失败时会返回无效的句柄 </returns>
-		Texture2DHandle AddTexture2D(const std::string& name, const std::string& path, TextureType texture_type = TextureType::TYPE_Undefined, bool is_create_mipmaps = true);
+		IVulkan::Texture2DHandle AddTexture2D(const std::string& name, const std::string& path, TextureType texture_type = TextureType::TYPE_Undefined, bool is_create_mipmaps = true);
 		/// <returns> 失败时会返回无效的句柄 </returns>
-		Texture2DHandle AddTexture2D(const std::string& name,
+		IVulkan::Texture2DHandle AddTexture2D(const std::string& name,
 			VkImage image, VkImageView image_view,
 			std::uint32_t width, std::uint32_t height, std::uint32_t mip_levels = 1,
 			VkFormat format = VK_FORMAT_R8G8B8A8_SRGB);
-		Texture2DHandle UpdateTexture2D(Texture2DHandle& handle, const std::string& path, TextureType texture_type = TextureType::TYPE_Undefined, bool is_create_mipmaps = true);
+		IVulkan::Texture2DHandle UpdateTexture2D(IVulkan::Texture2DHandle& handle, const std::string& path, TextureType texture_type = TextureType::TYPE_Undefined, bool is_create_mipmaps = true);
 		// 自动销毁 VkImage 与 VkImageView
-		Texture2DHandle UpdateTexture2D(Texture2DHandle& handle,
+		IVulkan::Texture2DHandle UpdateTexture2D(IVulkan::Texture2DHandle& handle,
 			VkImage image, VkImageView image_view,
 			std::uint32_t width, std::uint32_t height, std::uint32_t mip_levels = 1,
 			VkFormat format = VK_FORMAT_R8G8B8A8_SRGB);
 		// 不自动销毁 VkImage 与 VkImageView
-		Texture2DHandle UpdateTexture2DWithoutDestory(Texture2DHandle& handle,
+		IVulkan::Texture2DHandle UpdateTexture2DWithoutDestory(IVulkan::Texture2DHandle& handle,
 			VkImage image, VkImageView image_view,
 			std::uint32_t width, std::uint32_t height, std::uint32_t mip_levels = 1,
 			VkFormat format = VK_FORMAT_R8G8B8A8_SRGB);
-		void DestroyTexture2D(Texture2DHandle& handle);
+		void DestroyTexture2D(IVulkan::Texture2DHandle& handle);
 
-		bool IsTextureReady(const Texture2DHandle& handle) const;
+		bool IsTextureReady(const IVulkan::Texture2DHandle& handle) const;
 		bool IsValid() const { return _is_valid; }
 
-		IVulkanTexture2DHandle GetVulkanTextureHandle(const Texture2DHandle& handle) const;
+		IVulkanTexture2DHandle GetVulkanTextureHandle(const IVulkan::Texture2DHandle& handle) const;
 
 		void QueueDestroy(VkImage image, VkImageView image_view);
 		void FlushDestroyQueue();
@@ -199,8 +175,8 @@ namespace INVENT
 	private:
 		void _init_default_image();
 		void _init_other();
-		void _insert_name_cache(const std::string& name, Texture2DHandle handle);
-		void _remove_name_cache_by_handle(const Texture2DHandle& handle);
+		void _insert_name_cache(const std::string& name, IVulkan::Texture2DHandle handle);
+		void _remove_name_cache_by_handle(const IVulkan::Texture2DHandle& handle);
 		void _update_texture_count();
 		void _transition_image_layout(VkCommandBuffer commond_buffer,
 			VkImage image,
@@ -218,7 +194,7 @@ namespace INVENT
 			VkDeviceSize buffer_offset = 0,
 			VkImageLayout initial_layout = VK_IMAGE_LAYOUT_UNDEFINED);
 
-		const Texture2DHandle _find_handle_from_cache(const std::string& name) const;
+		const IVulkan::Texture2DHandle _find_handle_from_cache(const std::string& name) const;
 		uint32_t _calculate_level_count(const int& width, const int& height) const
 		{
 			return static_cast<uint32_t>(std::floor(std::log2(std::max(width, height)))) + 1;
--- Engine/Private/IVulkan/VulkanConfig.h
+++ Engine/Private/IVulkan/VulkanConfig.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#include "IBitArray.h"
+
 #include <cstdint>
 #include <vulkan/vulkan.h>
 
@@ -11,18 +13,75 @@
 #endif // !GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 
-namespace IVulkan
+namespace INVENT
 {
-	constexpr std::uint32_t MAX_FRAMES_IN_FLIGHT = 2U;
-	constexpr std::uint32_t MAX_BINDLESS_TEXTURES = 500000U;
-	constexpr std::uint32_t DEF_BINDLESS_TEXTURES = 100000U;
-	constexpr std::uint32_t MAX_ALLOCATED_SETS = MAX_FRAMES_IN_FLIGHT;
 
-	struct PushConstants 
+
+	namespace IVulkan
 	{
-		VkDeviceAddress VertexAddress{}; // 顶点起始指针
-		VkDeviceAddress MaterualAddress{}; // 材质起始指针
-		glm::mat4 MVP;
-	};
+		constexpr std::uint32_t MAX_FRAMES_IN_FLIGHT = 2U;
+		constexpr std::uint32_t MAX_BINDLESS_TEXTURES = 500000U;
+		constexpr std::uint32_t DEF_BINDLESS_TEXTURES = 100000U;
+		constexpr std::uint32_t MAX_ALLOCATED_SETS = MAX_FRAMES_IN_FLIGHT;
 
-}
+		struct PushConstants
+		{
+			VkDeviceAddress VertexAddress{ 0 };		// 顶点起始指针
+			VkDeviceAddress MaterualAddress{ 0 };	// 材质起始指针
+			VkDeviceAddress IndexAddress{ 0 };		// 顶点索引起始指针
+			glm::mat4 MVP{ 1.0f };
+		};
+
+		struct Vertex
+		{
+			glm::vec3 Position{ 0.0f };
+			std::uint32_t padding1{ 0 };
+
+			glm::vec2 TexCoords{ 0.0f };
+			std::uint32_t HasTexCoords{ 0 };
+			std::uint32_t padding2{ 0 };
+
+			glm::vec4 Color{ 1.0f };
+
+			glm::vec3 Normal{ 0.0f };
+			std::uint32_t HasNormal{ 0 };
+
+			glm::vec3 Tangent{ 0.0f };			// 切线
+			std::uint32_t HasTangent{ 0 };
+
+			glm::vec3 Bitangent{ 0.0f };		// 双切线
+			std::uint32_t HasBitangent{ 0 };
+		};
+
+		struct Texture2DHandle
+		{
+			IHandle handle{};
+			std::uint64_t version{ 0 };
+
+			Texture2DHandle() = default;
+			constexpr Texture2DHandle(size_t h)
+				: handle(h)
+			{}
+			constexpr Texture2DHandle(size_t h, std::uint32_t v)
+				: handle(h), version(v)
+			{}
+			Texture2DHandle(const Texture2DHandle&) = default;
+			Texture2DHandle(Texture2DHandle&&) noexcept = default;
+
+			Texture2DHandle& operator=(const Texture2DHandle&) = default;
+			Texture2DHandle& operator=(Texture2DHandle&&) noexcept = default;
+
+			friend bool operator==(const Texture2DHandle& handle1, const Texture2DHandle& handle2)
+			{
+				return handle1.handle == handle2.handle &&
+					handle1.version == handle2.version;
+			}
+
+			bool IsValid() const noexcept { return handle.IsValid(); }
+		};
+
+
+
+	} // namespace IVulkan
+
+} // namespcae INVENT
